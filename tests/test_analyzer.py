98dfd56b139e41e4d9848d18670b0325e57a3219
Nathan Hui <ntlhui@gmail.com>
2023-12-10 23:44:13 -0800

    fix: Allows for empty dict in job cache

---
+++ tests/test_analyzer.py
@@ -0,0 +1,16 @@
+'''Tests analyzer load
+'''
+import json
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+from e4e_deduplication.analyzer import Analyzer
+
+
+def test_naive_jobfile():
+    with TemporaryDirectory() as tmpdir:
+        job_path = Path(tmpdir, 'job.json')
+        with open(job_path, 'w', encoding='utf-8') as handle:
+            json.dump({}, handle)
+        with Analyzer(None, job_path) as analyzer:
+            assert analyzer
