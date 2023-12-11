e0f819242e8ce95a8c52d366eb8fec990d082b49
Nathan Hui <ntlhui@gmail.com>
2023-12-10 23:45:51 -0800

    style: Fixes docstring

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -8,6 +8,8 @@ from e4e_deduplication.analyzer import Analyzer
 
 
 def test_naive_jobfile():
+    """Tests a jobfile with an empty dict
+    """
     with TemporaryDirectory() as tmpdir:
         job_path = Path(tmpdir, 'job.json')
         with open(job_path, 'w', encoding='utf-8') as handle:
