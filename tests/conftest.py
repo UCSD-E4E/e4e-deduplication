2cc2509cb648d4e7eec18ec6ecf960d0733b0fb1
Nathan Hui <ntlhui@gmail.com>
2023-12-17 19:26:55 -0800

    ci: adding support for nas unzip

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,63 @@
 '''Test Setup
 '''
+import json
+import os
+import shutil
 from pathlib import Path
 from tempfile import TemporaryDirectory
+from typing import Dict
 
 import pytest
+from nas_unzip.nas import nas_unzip
 from utils import create_random_file
 
 from e4e_deduplication.analyzer import Analyzer
+
+
+@pytest.fixture(name='creds', scope='session')
+def create_creds() -> Dict[str, str]:
+    """Obtains the credentials
+
+    Returns:
+        Dict[str, str]: Username and password dictionary
+    """
+    if Path('credentials.json').is_file():
+        with open('credentials.json', 'r', encoding='ascii') as handle:
+            return json.load(handle)
+    else:
+        value = os.environ['NAS_CREDS'].splitlines()
+        assert len(value) == 2
+        return {
+            'username': value[0],
+            'password': value[1]
+        }
+
+
+@pytest.fixture(name='big_hash_cache', scope='session')
+def get_big_hash_cache(creds) -> Path:
+    """Retrieves the big hash cache
+
+    Args:
+        creds (Dict[str, str]): NAS Credentials
+
+    Returns:
+        Path: Path to data
+
+    Yields:
+        Iterator[Path]: Path to data
+    """
+    with TemporaryDirectory() as tmp_dir:
+        path = Path(tmp_dir).resolve()
+        if not Path('hashes.csv').is_file():
+            nas_unzip(
+                network_path='smb://e4e-nas.ucsd.edu:6021/temp/github_actions/e4e-deduplication/big_hash_cache.zip',
+                output_path=path,
+                username=creds['username'],
+                password=creds['password']
+            )
+        else:
+            shutil.copy(Path('hashes.csv'), path / 'hashes.csv')
+        yield path
 
 
 @pytest.fixture(name='test_analyzer')
